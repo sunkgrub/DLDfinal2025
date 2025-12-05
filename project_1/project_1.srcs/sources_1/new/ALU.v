d125f51fdf443c0fe9b6910b9790ed8cc0babb8c
sunkgrub <sunkgrub@gmail.com>
2025-12-05 01:00:21 -0500

    Created ALU

---
+++ project_1/project_1.srcs/sources_1/new/ALU.v
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/05/2025 12:30:57 AM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+    input [15:0] L,
+    input [15:0] R,
+    input [2:0] op,
+    output [31:0] out
+    );
+
+wire [31:0] productW;
+wire [15:0] sumW, diffW;
+
+adder16bit add0(
+.A(L),
+.B(R),
+.S(sumW)
+);
+
+subtractor16bit sub0(
+.A(R),
+.B(L),
+.D(diffW)
+);
+
+multiplier16bit m0(
+.A(L),
+.B(R),
+.P(productW)
+);
+
+multibit_mux #(.NUM_INPUTS(8), .BUS_WIDTH(32)) MBMUX (
+.data_out(out),
+.select_in(op),
+.data_in({{16'b0,R}, {16'b0,R}, productW, {16'b0,diffW}, {16'b0,sumW} ,{16'b0,L}, 32'b0, {16'b0,R}})
+);
+
+endmodule
